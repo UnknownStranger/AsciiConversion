fixed scaling

ascii.py
@@ -6,7 +6,7 @@ ascii = asciiString.split()
 
 #storing as np array
 #importing image and converting to greyscale
-im = np.array(Image.open("testimage.jpg").convert('L'))
+im = np.array(Image.open("DuckTales_28Main_title29.png").convert('L'))
 #adjusting brightness to character amount
 im = im/3.93
 im = np.round(im, decimals =  0)
@@ -14,13 +14,13 @@ imx = im.astype(int)
 #creating empty string to push ascii art into
 art = ""
 #iterating array and adding the related brightness character to the art string
-for x in imx:
+for x in imx[::2]:
   art += "\n"
   for y in x:
     art += ascii[y -1]
 
-print(art)
+#print(art)
 #saving the art as a text file
-#f = open("output.txt", "a")
-#print(art, file=f)
-#f.close()
+f = open("output.txt", "a")
+print(art, file=f)
+f.close()